50243d86c51187449eeed6ecbcb0c63a8152baf4
Felipe Torlai <felipe.c.torlai@gmail.com>
2024-06-04 11:54:51 -0300

    alterações

--- app/src/main/java/com/example/oceantech/ui/MainActivity.kt
+++ app/src/main/java/com/example/oceantech/ui/MainActivity.kt
@@ -1,11 +1,20 @@
-package com.example.oceantech
+package com.example.oceantech.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.oceantech.R
+import com.example.oceantech.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 }
